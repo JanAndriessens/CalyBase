f1827a470979a2fab27231df062712f4c67ad83f
JanAndriessens <jan.andriessens@gmail.com>
2025-07-10 14:44:01 +0200

    bug fix

--- src/config/env.js
+++ src/config/env.js
@@ -11,12 +11,12 @@ dotenv.config({ path: join(__dirname, '../../.env') });
 
 // Validate required environment variables
 const requiredEnvVars = [
-    'FIREBASE_API_KEY',
-    'FIREBASE_AUTH_DOMAIN', 
-    'FIREBASE_PROJECT_ID',
-    'FIREBASE_STORAGE_BUCKET',
-    'FIREBASE_MESSAGING_SENDER_ID',
-    'FIREBASE_APP_ID'
+    'NEXT_PUBLIC_FIREBASE_API_KEY',
+    'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN', 
+    'NEXT_PUBLIC_FIREBASE_PROJECT_ID',
+    'NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET',
+    'NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID',
+    'NEXT_PUBLIC_FIREBASE_APP_ID'
 ];
 
 const missingVars = requiredEnvVars.filter(varName => !process.env[varName]);
@@ -27,19 +27,19 @@ if (missingVars.length > 0) {
 // Export configuration - ONLY use environment variables
 export const config = {
     firebase: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
     },
     recaptcha: {
-        siteKey: process.env.RECAPTCHA_SITE_KEY || '',
-        secretKey: process.env.RECAPTCHA_SECRET_KEY || ''
+        siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '',
+        secretKey: process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY || ''
     },
     session: {
-        secret: process.env.SESSION_SECRET
+        secret: process.env.NEXT_PUBLIC_SESSION_SECRET
     }
 }; 
